71ee518144a9ea4a852bf8056d5680f63c88e739
mpiza1996 <michell.piza@degeremcia.com>
2023-05-17 16:29:53 -0500

    ACT17052023

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -34,8 +34,8 @@
                             <th width="500px" style="text-align: center">Asunto</th>
                             <th width="120px" style="text-align: center">Fecha de Creación</th>
                             <th width="120px" style="text-align: center">Fecha de Solicitud</th>
-                            <th width="200px" style="text-align: center">Departamento</th>
-                            <th width="500px" style="text-align: center">Usuario Solicitante</th>
+                            <th width="500px" style="text-align: center">Departamento - Encargado</th>
+                            <th width="300px" style="text-align: center">Solicitante</th>
                             <th width="250px" style="text-align: center">Estado</th>
                             <th width="100px" style="text-align: center">Acciones</th>
                         </tr>
@@ -44,13 +44,14 @@
                     <tbody style="text-align: center">
                         @foreach($tasks as $prodc)
                         <tr>
+
                             <td>{{ $prodc->id }}</td>
                             <td>{{ $prodc->asunto }}</td>
                             <td> <?php echo date('d/m/Y', strtotime($prodc->created_at)); ?></td>
                             <td> <?php echo date('d/m/Y', strtotime($prodc->fecha_entrega)); ?></td>
-                            <td>{{ $prodc->namedt }}</td>
+                            <td>{{ $prodc->namedt }} - {{ $prodc->ApellidoAsig }} {{ $prodc->NombreAsig }}</td>
                             <td>{{ $prodc->ApellidoSoli }} {{ $prodc->NombreSoli }}</td>   
-                            <td>{{ $prodc->estado }}</td>   
+                            <td>{{ $prodc->estado }}</td>  
                             <td>
                               @if($prodc->usuario_solicitante == Auth::user()->id)
                               <div class="text-center">
