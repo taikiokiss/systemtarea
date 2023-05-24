c626bc761d254a33228ecbc7d8932ccec14f0bec
mpiza1996 <michell.piza@degeremcia.com>
2023-05-24 17:03:45 -0500

    ACT24052023

--- resources/views/print/src/header.blade.php
+++ resources/views/print/src/header.blade.php
@@ -1,7 +1,26 @@
 <div id="header">
   <div class="container">
     <div class="row">
-      <div class="col-md-12" style="margin-left: 280px;">
+      <div class="col-md-12"  style="margin-left: 50px;margin-right: 50px;">
+          <table class="responsive" style="font-size:11px;">
+            <tr>
+              <th colspan="4" style="font-size:14px;"><em>DETALLE DE CUMPLIMIENTO</em></th>
+            </tr>
+            <tr>
+              <td style="width:30%; text-align: left;"><b>Fecha Inicio:</b></td>
+              <td style="width:20%"> --------</td>
+              <td style="width:30%;text-align: left";><b>Fecha Fin:</b></td>
+              <td style="width:20%"> --------</td>
+            </tr>
+            <tr>
+              <td style="width:30%;text-align: left";><b>Usuario:</b></td>
+              <td colspan="3" style="width:70%"> --------</td>
+            </tr>
+            <tr>
+              <td style="width:30%;text-align: left";><b># Tarea:</b></td>
+              <td colspan="3" style="width:70%"> --------</td>
+            </tr>
+          </table>
       </div>
     </div>
   </div>
