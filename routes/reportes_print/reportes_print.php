bd2ee179505c33bdb63f34f5d00b8edd5481e250
mpiza1996 <michell.piza@degeremcia.com>
2023-06-23 14:30:28 -0500

    Update reportes_print.php

--- routes/reportes_print/reportes_print.php
+++ routes/reportes_print/reportes_print.php
@@ -10,11 +10,11 @@ Route::middleware(['auth'])->group(function () {
 
 	Route::get('tareas/reporte/resumido', 'ReporteController@report_resumido')
 		->name('reportes.report_resumido')
-		->middleware('role:ADMINISTRADOR|USUARIO');
+		->middleware('role:ADMINISTRADOR|USUARIO|SUPER-ADMIN');
 
 	Route::get('tareas/reporte/grafica', 'ReporteController@report_grafica')
 		->name('reportes.report_grafica')
-		->middleware('role:ADMINISTRADOR|USUARIO');
+		->middleware('role:ADMINISTRADOR|USUARIO|SUPER-ADMIN');
 
 	Route::get('/mps-data', 'ReporteController@getMpsData');
 
@@ -23,11 +23,11 @@ Route::middleware(['auth'])->group(function () {
 
 	Route::get('tareas/print/reporte_resumido/', 'ReporteController@imprimir_reporte_resumido')
 		->name('print.reporte_resumido')
-		->middleware('role:ADMINISTRADOR|USUARIO');
+		->middleware('role:ADMINISTRADOR|USUARIO|SUPER-ADMIN');
 
 	Route::get('tareas/print/reporte_detallado/{data}', 'ReporteController@imprimir_reporte_detallado')
 		->name('print.reporte_detallado')
-		->middleware('role:ADMINISTRADOR|USUARIO');
+		->middleware('role:ADMINISTRADOR|USUARIO|SUPER-ADMIN');
 		
 });
 ?>
