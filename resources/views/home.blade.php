70883e26d612071e7e30133c4d3851906825f18a
mpiza1996 <michell.piza@degeremcia.com>
2023-07-28 12:09:24 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,12 @@
 			<div class="card-header"><h5><span class="text-center fa fa-home"></span> @yield('title')</h5></div>
 			<div class="card-body">
 				<hr>
+
+
+                
 					<div class="row w-100">
+
+
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-danger shadow h-100 py-2">
                                 <div class="card-body">
@@ -81,6 +86,8 @@
                             </div>
                         </div>
 
+
+
                         <!-- Content Column -->
                         <div class="col-lg-12 col-md-6 mb-4">
 
@@ -129,5 +136,7 @@
 		</div>
 	</div>
 </div>
+
+
 </div>
 @endsection
